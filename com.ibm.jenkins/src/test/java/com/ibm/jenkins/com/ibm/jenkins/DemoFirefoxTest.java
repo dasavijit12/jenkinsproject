a31dcba1d5af0cba1b739342cb193cc904d0ebaf
ibm <ibm@9.78.194.97>
2019-01-04 16:01:48 +0530

    running firefox & chrome

--- com.ibm.jenkins/src/test/java/com/ibm/jenkins/com/ibm/jenkins/DemoFirefoxTest.java
+++ com.ibm.jenkins/src/test/java/com/ibm/jenkins/com/ibm/jenkins/DemoFirefoxTest.java
@@ -10,10 +10,11 @@ public class DemoFirefoxTest {
 	
 	WebDriver driver;
 	
-	@Test(enabled=false)
+	@Test
 	public void demoFirefoxTest() {
 		String baseUrl = "http://realestate.hommelle.com";
-		
+		System.out.println("***************This is firefox testing***************");
+		System.out.println("***************This is before Launching Browser***************");
 		System.setProperty
 		("webdriver.gecko.driver", "C:\\Users\\IBM_ADMIN\\Downloads\\selenium\\geckodriver-v0.20.1-win64\\geckodriver.exe");
 		DesiredCapabilities cap = DesiredCapabilities.firefox();
@@ -22,6 +23,7 @@ public class DemoFirefoxTest {
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get(baseUrl);
+		System.out.println("***************This is after Launching Browser***************");
 		
 		String expectedTitle = "Real Estate";
 		String actualTitle = driver.getTitle();
--- com.ibm.jenkins/src/test/java/com/ibm/jenkins/com/ibm/jenkins/NormalDemoTest.java
+++ com.ibm.jenkins/src/test/java/com/ibm/jenkins/com/ibm/jenkins/NormalDemoTest.java
@@ -12,12 +12,15 @@ public class NormalDemoTest {
 	@Test
 	public void demoTest() {
         String baseUrl = "http://realestate.hommelle.com";
-		System.out.println("Hello...");
+
+        System.out.println("***************This is Chrome testing***************");
+		System.out.println("***************This is before Launching Browser***************");
+
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(baseUrl);
-		
+		System.out.println("***************This is after Launching Browser***************");
 		String expectedTitle = "Real Estate";
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(expectedTitle, actualTitle);
